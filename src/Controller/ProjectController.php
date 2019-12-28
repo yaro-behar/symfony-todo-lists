2b2f59c2844003f0bb1cb8e95d3f8d2f2a8aa5b0
jarique <jarikbegar@ukr.net>
2019-12-28 16:50:53 +0200

    pa

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use App\Entity\Project;
@@ -36,6 +37,24 @@ class ProjectController extends AbstractController
             Response::HTTP_OK,
             ['content-type' => 'text/html']
         );
+    }
+
+    /**
+     * @Route("/project/update", name="project-update", methods={"POST"})
+     */
+    public function update(Request $request)
+    {
+        $manager = $this->getDoctrine()->getManager();
+
+        $project = $manager->getRepository(Project::class)->find($request->request->get('project_id'));
+        if (!$project) {
+            throw $this->createNotFoundException('No project found for id ' . $request->request->get('project_id'));
+        }
+
+        $project->setName($request->request->get('project_name'));
+        $manager->flush();
+
+        return new JsonResponse(null, Response::HTTP_OK);
     }
 
     /**
