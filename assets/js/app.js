98367638641882e25bf9a6b24728b2b6671f9b0f
jarique <jarikbegar@ukr.net>
2020-01-01 16:09:55 +0200

    Mark Task as 'Done'

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ import $ from 'jquery';
 window.jQuery = $;
 window.$ = $;
 
+import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
