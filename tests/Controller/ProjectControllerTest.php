e41d6843e73994a98f31ce5546b7740dbdd101f7
jarique <jarikbegar@ukr.net>
2020-01-26 16:53:43 +0200

    pa

--- tests/Controller/ProjectControllerTest.php
+++ tests/Controller/ProjectControllerTest.php
@@ -4,6 +4,8 @@ namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\QueryBuilder;
 use App\Entity\Project;
 
 class ProjectControllerTest extends WebTestCase
@@ -49,7 +51,7 @@ class ProjectControllerTest extends WebTestCase
 
         $this->login($client);
 
-        $projectRepository  = $client->getContainer()->get('doctrine')->getRepository(Project::class);
+        $projectRepository = $client->getContainer()->get('doctrine')->getRepository(Project::class);
 
         $projectsBeforeInsert = $projectRepository->count([]);
 
@@ -60,9 +62,53 @@ class ProjectControllerTest extends WebTestCase
         $this->assertEquals($projectsBeforeInsert + 1, $projectsAfterInsert);
     }
 
+    public function testUpdateProject()
+    {
+        // TODO: implement
+    }
+
     public function testDeleteProject()
     {
+        $client = static::createClient();
 
+        $client->request('GET', '/login');
+
+        $this->login($client);
+
+        $projectRepository = $client->getContainer()->get('doctrine')->getRepository(Project::class);
+
+        $projectsBeforeDelete = $projectRepository->count([]);
+
+        $client->xmlHttpRequest('GET', '/project/delete/' . $projectRepository->findOneBy([])->getId());
+
+        $projectsAfterDelete = $projectRepository->count([]);
+
+        $this->assertEquals($projectsBeforeDelete - 1, $projectsAfterDelete);
+    }
+
+    public function testDeleteNonExistentProject()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/login');
+
+        $this->login($client);
+
+        /** @var QueryBuilder $queryBuilder */
+        $queryBuilder = $client->getContainer()
+            ->get('doctrine')
+            ->getRepository(Project::class)
+            ->createQueryBuilder('p');
+        $ids = array_column($queryBuilder->select('p.id')->getQuery()->getArrayResult(), 'id');
+
+        $nonExistentId = 1;
+        while (in_array($nonExistentId, $ids)) {
+            $nonExistentId++;
+        }
+
+        $client->xmlHttpRequest('GET', '/project/delete/' . $nonExistentId);
+
+        $this->assertEquals(Response::HTTP_NOT_FOUND, $client->getResponse()->getStatusCode());
     }
 
     private function login(KernelBrowser $client)
